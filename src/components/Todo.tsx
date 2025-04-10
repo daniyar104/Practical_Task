75882b71511f4cfd81e817bc9f2101d04ab3f2b0
Daniyar <ukubidoma@gmail.com>
2025-04-10 21:53:57 +0500

    (add): Add Task display logic

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,5 +1,6 @@
 "use client"
 import {useState} from "react";
+import styles from "./todo.module.css";
 
 interface ITodo {
     id: string;
@@ -8,16 +9,31 @@ interface ITodo {
 }
 
 const Todo: React.FC = () => {
-    const [todo, setTodo] = useState<string>("")
+    const [todos, setTodo] = useState<ITodo[]>([])
+    const [newTask, setNewTask] = useState<string>("")
     const showTodo = () => {
-        console.log(todo)
-        setTodo(" ")
+        console.log(newTask)
+        setNewTask(" ")
     }
     return(
         <div>
             <h1>Список задач</h1>
-            <input onChange={event => setTodo(event.target.value)}/>
-            <button onClick={showTodo}>Add</button>
+            <div>
+                <input type="text" value={newTask} onChange={event => setNewTask(event.target.value)}/>
+                <button onClick={showTodo}>Add</button>
+            </div>
+            <div>
+                {
+                    todos.length === 0 ?
+                        <h1>Tasks is null</h1>
+                        :
+                        todos.map(todo => (
+                            <div key={todo.id}>
+                                <span>{todo.name}</span>
+                            </div>
+                        ))
+                }
+            </div>
         </div>
     )
 }
